7f66bf24858066c534c61c3a28e901fdd0ad4f50
Mohamed Gudle <gudlemohamed@gmail.com>
2025-06-12 12:19:54 +0300

    fix typeorm error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { User, Customer, Creator, Factory, Admin } from './users/entities';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [User, Customer, Creator, Factory, Admin],
+        synchronize: configService.get<string>('NODE_ENV') === 'development',
+        logging: configService.get<string>('NODE_ENV') === 'development',
+        ssl: configService.get<string>('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
     UsersModule,
